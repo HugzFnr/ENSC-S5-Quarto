ee1b3d9f293653e5342c779e803b4bcee6191327
hfournier001 <hfournier001@ensc.fr>
2018-12-04 11:33:58 +0100

    création tableaux code et symbole

--- Quarto!/Quarto1/Program.cs
+++ Quarto!/Quarto1/Program.cs
@@ -8,11 +8,36 @@ namespace Quarto1
 {
     class Program
     {
+        int a = 0;
         static void Main(string[] args)
         {
-            //WESH GUIGUITE
+            //partie test
+            //     Console.Title{; "Quarto!"; }
             Console.WriteLine("TEST2");
             int plop = 2;
+            string test = "ABCD";
+            Console.WriteLine(test[0]);
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("White on blue.");
+            Console.WriteLine("Another line.");
+
+            // fin de la partie test
+
+            string[] codePieces = new string[]
+            {
+                "PBRV","PBRE","PBCV","PBCE",
+                "PNRV","PNRE","PNCV","PNCE",
+                "GBRV","GBRE","GBCV","GBCE",
+                "GNRV","GNRE","GNCV","GNCE"
+            };
+
+            string[,] symbolePieces = new string[,]
+            {
+               { "    ", "    ", "    ", "    ", "    ", "    ", "    ", "    ", "(  )","(())","[  ]","[[]]","(  )","(())","[  ]","[[]]" },
+               { "(  )", "(())", "[  ]", "[[]]", "(  )", "(())", "[  ]", "[[]]", "(  )","(())","[  ]","[[]]","(  )","(())","[  ]","[[]]" }
+
+            };
         }
     }
 }
